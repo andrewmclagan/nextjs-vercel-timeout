2348909511cb23e57c5233da9ee778ff718b8273
Andrew McLagan (Rew) <andrewmclagan@gmail.com>
2024-01-15 16:17:03 +1100

    latest

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,9 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   console.log("TEST");
   try {
-    return NextResponse.next();
+    const resp = NextResponse.next();
+    console.log(resp);
+    return resp;
   } catch (error: unknown) {
     console.log("ERROR!!!!");
     console.error("ERROR!!!!");
