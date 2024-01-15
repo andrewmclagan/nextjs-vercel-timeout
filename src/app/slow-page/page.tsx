8e65c33248820d04df1d54f0d0b640569f06d04f
Andrew McLagan (Rew) <andrewmclagan@gmail.com>
2024-01-15 15:50:33 +1100

    latest

--- src/app/slow-page/page.tsx
+++ src/app/slow-page/page.tsx
@@ -2,10 +2,10 @@ import { cache } from "react";
 
 const TIMEOUT_SECONDS = 5;
 
+export const dynamic = "force-dynamic";
+
 const getTime = cache(async (): Promise<number> => {
-  await new Promise((resolve) =>
-    setTimeout(() => resolve(1), 5 * 1000)
-  );
+  await new Promise((resolve) => setTimeout(() => resolve(1), 5 * 1000));
   return new Date().getTime();
 });
 
